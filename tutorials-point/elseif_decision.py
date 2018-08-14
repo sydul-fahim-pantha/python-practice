a930e260a2744f8265b2d01530b8b62f642430ee
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-14 06:14:19 +0600

    Decision making using if and elseif

---
+++ tutorials-point/elseif_decision.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# elseif
+a = 100
+b = 10
+print("a " + str(a) + "  b " + str(b))
+
+if (a == b) : 
+  print("a==b if executed")
+  print("a==b if ended")
+elif (a > b) :
+  print("a>b else")
+  print("a>b else ended")
+elif (a < b) :
+  print("a<b else")
+  print("a<b else ended")
+else :
+  print("default else ")
+
+print("End of code")
+ 
+
+
---
+++ tutorials-point/ifelse_decision.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# single if 
+a = 100
+if (a == 100) : print("a == 100 if executed ")
+
+
+# if-else
+
+b = 10
+print("a " + str(a) + "  b " + str(b))
+
+if (a == b) : 
+  print("a==b if executed")
+  print("a==b if ended")
+else :
+  print("a==b else")
+  print("a==b else ended")
+
+
+c = 10
+print("a " + str(a) + "  c " + str(c))
+
+if (a != c) : 
+  print("a!=c if executed")
+  print("a!=c if ended")
+else :
+  print("a<>c else")
+  print("a<>c else ended")
+ 
+
+
