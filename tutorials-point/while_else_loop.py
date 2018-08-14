d73a8fccf2789bc40e2e4757d2e9fa1dd61af128
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-14 06:55:05 +0600

    While using else

---
+++ tutorials-point/while_else_loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+count = 0
+
+# else will execute when leaving the while loop
+while count < 9:
+   print('The count is:', count)
+   count+=1;
+else : print("else executed")
+
+
+
+
--- tutorials-point/while_loop.py
+++ tutorials-point/while_loop.py
@@ -3,7 +3,7 @@
 count = 0
 
 print(">>>>>>>>>>>>>>>> simple count loop started >>>>>>>>>>>>")
-while (count < 9):
+while count < 9:
    print('The count is:', count)
    count+=1;
 
@@ -11,16 +11,26 @@ print("Good bye!")
 print(">>>>>>>>>>>>>>>> simple count loop ended >>>>>>>>>>>>")
 
 
+print("\n\n>>>>>>>>>>>>>>>> break loop started >>>>>>>>>>>>")
+while True :
+   print("\nWrite \"exit\" to exit")	
+   a = input("Enter anything:")
+   print("You entered: ", a)
+   if a == "exit": 
+      print("Exiting using if") 
+      break; 
+print(">>>>>>>>>>>>>>>> break loop ended >>>>>>>>>>>>")
+
+
+
 print("\n\n>>>>>>>>>>>>>>>> infinite loop started >>>>>>>>>>>>")
 a = 10
 while a != "exit":
-   
    print("\nWrite \"exit\" to exit")	
-   a = input("Enter a num:")
+   a = input("Enter anything:")
    print("You entered: ", a)
-
-print("Good bye!")
 print(">>>>>>>>>>>>>>>> infinite loop ended >>>>>>>>>>>>")
+
 
 
 
