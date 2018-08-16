e52156f412b8699319b85ad6cfcb137ecf838f4b
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-16 07:07:46 +0600

    Math module related important function

---
+++ tutorials-point/math_module.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import math
+
+print("math.exp(2): ", math.exp(2))
+print("math.fabs(-2.45): ", math.fabs(-2.45))
+
+print()
+print("math.ceil(2.45): ", math.ceil(2.45))
+print("math.ceil(-2.45): ", math.ceil(-2.45))
+
+print()
+print("math.floor(2.45): ", math.floor(2.45))
+print("math.floor(-2.45): ", math.floor(-2.45))
+
+print()
+print("math.log(10): ", math.log(10))
+print("math.log10(10): ", math.log10(10))
+
+print()
+print("max(1, 2, 3): ", max(1, 2, 3))
+print("max(range(4)): ", max(range(4)))
+
+print()
+print("min(1, 2, 3): ", min(1, 2, 3))
+print("min(range(4)): ", min(range(4)))
+print("min(range(1,4)): ", min(range(1,4)))
+
+print()
+print("math.modf(10.5): ", math.modf(10.5))
+print("math.modf(-10.5): ", math.modf(-10.5))
+
+print()
+print("math.pow(10, 1): ", math.pow(10, 1))
+print("math.pow(10, -1): ", math.pow(10, -1))
+print("math.pow(10, 2): ", math.pow(10, 2))
+print("math.pow(10, -2): ", math.pow(10, -2))
+
+print()
+print("round(10.56): ", round(10.56))
+print("round(-10.56): ", round(-10.56))
+print("round(10.567, 1): ", round(10.567, 1))
+print("round(-10.567, 1): ", round(-10.567, 1))
+
+print()
+print("math.sqrt(25): ", math.sqrt(25))
+
+
