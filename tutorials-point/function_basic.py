3a3c66c5e191ad0421c54a807fc39426aa59f3e9
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-19 17:54:46 +0600

    Basic structure of a function

---
+++ tutorials-point/function_basic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+print()
+def func1(): 
+    print("simple function")
+    return
+
+def func2(str): 
+    print("arg str: ", str)
+    return
+
+def func3(): return "func3 output"
+
+func1()
+
+func2("str arg value")
+
+output = func3()
+print(output)
+
+
+
+
+
