41d0e26101371877f67e1b0e2e48d7be5f58d9fa
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-19 12:14:45 +0600

    tuple related function and operation

---
+++ tutorials-point/tuple_basic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print()
+print("Tuple is a sequence of immutable list")
+t1 = (1,2,3,4)
+print("t1: ", t1)
+
+
+print()
+print("Indexing, slicing and matrixes are same as list")
+
