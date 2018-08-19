3063f7203419d1c56fa4f2aab57ccee8bd68737d
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-19 18:27:34 +0600

    Anonymous function

---
+++ tutorials-point/function_anonymous.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print()
+print("An anonymous function is known as lamda. It's a one line version of a function")
+print("sum = lambda arg1, arg2, arg3 : arg1 + arg2 + arg3")
+print("Invoke: sum(10, 20, 30)")
+
+sum = lambda arg1, arg2, arg3 : arg1 + arg2 + arg3
+print(sum(10, 20, 30))
