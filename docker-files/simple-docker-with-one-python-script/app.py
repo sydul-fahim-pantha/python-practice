6bbd68125196b16d66d387d93098f70d25bd9207
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2018-08-29 07:34:05 +0600

    Simple docker file with one python script as an app

---
+++ docker-files/simple-docker-with-one-python-script/app.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a+b;
+
+def mult(a,b):
+    return a*b;
+
+if __name__ == "__main__":
+    print("+================+")
+    print("|  Hello world!  |")
+    print("+================+")
