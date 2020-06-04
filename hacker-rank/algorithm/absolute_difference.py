394d8d86513ce70c493f6277298ce8aa8ff2f9e6
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2020-06-04 12:10:58 +0600

    Minimum Absolute Difference in an Array

---
+++ hacker-rank/algorithm/absolute_difference.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+   
+    last_diff = abs(arr[0] - arr[1])
+    for i in range(1, len(arr) - 1):
+        diff =  abs(arr[i] - arr[i+1])
+        if diff < last_diff: last_diff = diff
+    
+    return last_diff
+
+if __name__ == '__main__':
+    fptr = open('/home/sydul/Work/all_git_repos/personal_sydul_fahim_pantha/python-practice/hacker-rank/algorithm/output_file.txt', 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
